ff1248bdbd5381c76f314257a9ea7847ef6e39da
sabyasachimohapatra <mohapatrasabyasachi162004@gmail.com>
2025-01-31 08:54:54 +0530

    added some configuration of cookie in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -108,6 +108,9 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
     .cookie("token", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      sameSite: "None",
+      secure: true
+
     })
     .json({
       success: true,
